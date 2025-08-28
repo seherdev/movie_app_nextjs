6b1169259fba4d448bb0d07b115b60c1cde2b058
seherdev <7iletisimseherarslanbay@gmail.com>
2025-08-28 14:48:43 +0300

    proje düzeni

---
+++ app/layout.tsx
@@ -0,0 +1,2 @@
+//genel layout. app>layout.tsx
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,2 +1,17 @@
+//genel page. app>page.tsx
+
+
 import "tailwindcss";
 import "autoprefixer";
+
+
+  <html lang="en">
+    <head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Document</title>
+    </head>
+      <body>
+        
+      </body>
+  </html>

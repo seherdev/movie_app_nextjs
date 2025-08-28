797b83ca2e678be81522fec3c5a5a9cb530d3eaf
seherdev <7iletisimseherarslanbay@gmail.com>
2025-08-28 16:51:04 +0300

    7.2

--- lib/omdb.ts
+++ lib/omdb.ts
@@ -1,12 +1,22 @@
-// lib/omdb.ts
-export async function fetchMovie(title: string) {
-  const apiKey = process.env.NEXT_PUBLIC_OMDB_API_KEY;
-  const res = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&t=${title}`);
+import { Movie } from '@/types/types';
 
-  if (!res.ok) throw new Error("Film verisi alınamadı");
+const API_KEY = process.env.NEXT_PUBLIC_OMDB_API_KEY;
+const BASE_URL = 'https://www.omdbapi.com/';
 
+export async function fetchMovie(title: string): Promise<Movie> {
+  const res = await fetch(`${BASE_URL}?t=${title}&apikey=${API_KEY}&plot=full`);
+  
+  if (!res.ok) throw new Error('API request failed');
+  
   const data = await res.json();
+
   if (data.Response === "False") throw new Error(data.Error);
 
-  return data;
+  return {
+    Title: data.Title,
+    Year: data.Year,
+    Poster: data.Poster,
+    Genre: data.Genre,
+    imdbID: data.imdbID,
+  };
 }

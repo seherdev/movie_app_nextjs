27e40ae01d0c4734008cd060fe50448049944591
seherdev <7iletisimseherarslanbay@gmail.com>
2025-09-02 11:15:31 +0300

    26 API

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-white shadow mt-10">
       <div className="max-w-5xl mx-auto p-4 text-center text-sm text-gray-500">
-        &copy; 2025 My Movie App. All rights reserved.
+        &copy; 2025 My Movie App.
       </div>
     </footer>
   );
--- src/lib/omdb.ts
+++ src/lib/omdb.ts
@@ -5,7 +5,7 @@ import { Movies } from '@/src/types/types';
 const API_URL = "https://www.omdbapi.com/";
 
 
-const API_KEY = process.env._NEXT_PUBLIC_OMDB_API_KEY;
+const API_KEY = process.env.NEXT_PUBLIC_OMDB_API_KEY;
 const BASE_URL = 'https://www.omdbapi.com/';
 
 export async function fetchMovies(searchTerm: string): Promise<Movies[] | null> {
